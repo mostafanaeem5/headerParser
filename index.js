d4edf1ef9502e91476fa6fd04254e49e48c5c4b8
mostafanaeem5 <72983247+mostafanaeem5@users.noreply.github.com>
2022-10-12 13:40:42 +0200

    update index.js

--- index.js
+++ index.js
@@ -16,15 +16,20 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
+app.enable('trust proxy');
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+    res.json({
+        ipaddress: req.ip,
+        language: req.headers['accept-language'],
+        software: process.platform,
+    });
 });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+    console.log('Your app is listening on port ' + listener.address().port);
 });
